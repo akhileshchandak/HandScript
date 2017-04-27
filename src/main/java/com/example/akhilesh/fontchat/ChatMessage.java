234f4129bfa3649a97bf80c2302cb4d7adb6cf4a
simrantalda <simran.talda04@gmail.com>
2017-04-27 14:02:35 +0530

    Add files via upload

---
+++ src/main/java/com/example/akhilesh/fontchat/ChatMessage.java
@@ -0,0 +1,16 @@
+package com.example.akhilesh.fontchat;
+
+/**
+ * Created by devesh on 02-04-2017.
+ */
+public class ChatMessage {
+    public boolean left;
+    public String message;
+
+    public ChatMessage(boolean left , String message) {
+        // TODO Auto-generated constructor stub
+        super();
+        this.left=left;
+        this.message = message;
+    }
+}
